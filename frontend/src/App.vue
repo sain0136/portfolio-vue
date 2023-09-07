90f47e4f1db7d8c6259e3d5adaded084a86d2f6a
sain0136 <55252053+sain0136@users.noreply.github.com>
2023-09-07 00:56:01 -0400

    feat: Added initial libs and design

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-
+import { useLanguage } from "@/languages/useLanguage";
+const { langTranslations } = useLanguage();
 </script>
 
 <template>
-<h1 class=" text-blue-500 text-4xl text-center">sup</h1>
-
+  <h1 class="text-blue-500 text-4xl text-center">{{ langTranslations.demoText }}</h1>
+  <router-view></router-view>
 </template>
 
-<style lang="scss" scoped>
-h1{
-  font-style: italic;
-}
-</style>
+<style lang="scss" scoped></style>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
